02a82bdcbb8c02aab7373f386bb85d1470229f9c
ksamedov1998 <ksamedov1998@gmail.com>
2020-03-27 22:42:37 +0400

    Country history implemented

--- api_client/src/test/java/io/service/api_client/ApiClientApplicationTests.java
+++ api_client/src/test/java/io/service/api_client/ApiClientApplicationTests.java
@@ -1,13 +1,13 @@
 package io.service.api_client;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiClientApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class ApiClientApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
